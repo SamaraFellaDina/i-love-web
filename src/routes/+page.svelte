6fc8ae5155c11788e0003dad4f0e1cc41dab98a3
SamaraFellaDina <sammybensink@gmail.com>
2024-09-15 23:48:34 +0200

    basis html invoegen voor de homepagina #65

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<body>
+<header>
+
+</header>
+
+<main>
+    <section>
+        <h1>archive</h1>
+        <p>
+            Welcome to my archive. Where you can find journals, 
+            notes, projects and even more! Take a look around :-)
+        </p>
+    </section>
+    <ul>
+        <li>
+            <h2>projects</h2>
+            <p>all my Github projects can be found here with some more information about the project, code and things like issues</p>
+            <a href="/">link</a>
+        </li>
+        <li>
+            <h2>learning journal</h2>
+            <p>a daily to weekly journal about my progress inside the projects.</p>
+            <a href="/">link</a>
+        </li>
+        <li>
+            <h2>we love web</h2>
+            <p>The place to find about presentations and the learning envenvironment  outside of the material that has been given by school</p>
+            <a href="/">link</a>
+        </li>
+        <li>
+            <h2>about me</h2>
+            <p>Who is the person behind all of this? Find more here!</p>
+            <a href="/">link</a>
+        </li>
+    </ul>
+
+</main>
+
+<footer>
+    <section> 
+        <h2>recent updates</h2>
+    </section>
+</footer>
+</body>
+
+<style>
+
+</style>
