ca610e57e803e9c2ace0a02271fc54fee2a22ca5
SamaraFellaDina <sammybensink@gmail.com>
2024-10-06 23:56:59 +0200

    maken van component en stijlen hiervan #92

---
+++ src/lib/homepage/heading.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<article>
+<section>
+  <h1>Archive</h1>
+  <div></div>
+</section>
+  <p>Welcome to my archive. Where you can find journals, notes, projects and even more! Take a look around :-)</p>
+</article>
+
+<style>
+
+article section {
+  display: flex;
+  justify-content: space-between;
+  gap: 1em;
+}
+
+article section div {
+  background: var(--lightmode-color-footer);
+  background: var(--lightmode-color-rainbow-gradient);
+  width: 100%;
+}
+
+</style>
