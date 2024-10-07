8e904d57978aa19662ffbf27b1522f195b026398
SamaraFellaDina <sammybensink@gmail.com>
2024-10-07 12:09:46 +0200

    invoegen van een component #92

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Heading } from './homepage/heading.svelte'
+export { default as Subjects } from './homepage/subjects.svelte'
