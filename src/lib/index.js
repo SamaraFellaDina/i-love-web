658ab8ca38c7320faf2139e02b2a82c1bc7b34d4
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-12-08 14:01:11 +0100

    Aanmaken van het astro framework #131

--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as Heading } from './homepage/heading.svelte'
-export { default as Subjects } from './homepage/subjects.svelte'
-export { default as Icons } from './icons.svelte'
