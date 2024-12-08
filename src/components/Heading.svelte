b43c84d162b8f7e7a2990dd956c77ea473b71626
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-12-08 15:21:07 +0100

    Invoegen van content in Astro #131
    Tijdens het maken ben ik erachter gekomen dat je svelte componenten gemakkelijk kan inladen. Hierin heb ik dat dus ook gedaan. Hierna wil ik kijken hoe ik `markdown` inlaad en stijling kan geven

---
+++ src/components/Heading.svelte
@@ -0,0 +1,37 @@
+
+<section>
+  <h1>Archive</h1>
+  <p>Welcome to my archive. Where you can find journals, notes, projects and even more! Take a look around :-)</p>
+</section>
+
+<style>
+
+section {
+  padding: var(--average-padding) 0;
+  position: relative;
+  width: 100vw;
+  height: 90vh;
+
+  background: var(--lightmode-color-footer);
+  background: var(--lightmode-color-rainbow-gradient);
+  animation: gradient 5s ease infinite alternate;
+  background-size: 400% 400%;
+}
+
+section h1 {
+  --heading-margin:.2em;
+  margin-block-start: var(--heading-margin);
+  margin-block-end: var(--heading-margin);
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0;
+  }
+
+  100% {
+    background-position: 100%;
+  }
+}
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+export { default as Footer } from './Layouts/Footer.svelte'
+export { default as Header } from './Layouts/Header.svelte'
+
+export { default as Heading } from './components/Heading.svelte'
+export { default as Subjects } from './components/Subjects.svelte'
+export { default as Icons } from './components/Icons.svelte'
+export { default as Navigation } from './components/Navigation.svelte'
+
+
+
+
