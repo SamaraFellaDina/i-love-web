8def9526f841510d0d2c6748a34a68d66c53f247
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2024-12-24 00:07:29 +0100

    Stijlen van de landingspagina
    ik ben nu bezig met de mobiele versie van de landingspagina. ik wil alleen nog voor de desktop versie iconen invoegen en daarin een media query invoegen

--- src/components/Subjects.svelte
+++ src/components/Subjects.svelte
@@ -7,8 +7,10 @@
 <ul>
   {#each archive as {title, description, slug}}
     <li>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <section>
+        <h2>{title}.</h2>
+        <p>{description}</p>
+      </section>
       <a href="/{slug}">
         <Icons 
         name = 'arrow-right'
@@ -35,12 +37,12 @@
     --border-style: solid;
     --border-color: var(--lightmode-color-black);
     --border-width: 2px;
-    --min-height: 5em;
 
     border-top-style: var(--border-style);
     border-color: var(--border-color);
     border-width: var(--border-width);
-    padding: var(--average-padding);
+    display: flex;
+    justify-content: space-between;
   }
 
   ul li:nth-child(1){
@@ -64,18 +66,36 @@
     border-width: var(--border-width);
   }
 
+  ul li section {
+    display: flex;
+    flex-direction: column;
+    min-height: 15em;
+  }
+  
+  ul li section h2,
+  ul li section p {
+    margin: .5em 0;
+  }
+
+  ul li section h2{
+    font-size: 4em;
+    margin: -15px -5px;
+  }
+
+  ul li section p {
+    padding: var(--average-padding);
+  }
+
   ul li a {
     --ratio: 2em;
-    padding: .5em;
-    display: flex;
     width: var(--ratio);
     height: var(--ratio);
+    padding: .5em;
+
+    display: flex;
     flex-direction: row;
-    justify-content: flex-end;
+    justify-content: center;
+
     background-color: var(--lightmode-color-black);
-  }
-  ul li h2,
-  ul li p {
-    margin: 1em 0;
   }
 </style>
