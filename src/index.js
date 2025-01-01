278fbf3dc4b02ea03d358ec49d0bf56f6dac0a28
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-01-01 17:40:27 +0100

    code meer DRY maken voor de subjects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 export { default as Footer } from './Layouts/Footer.svelte'
 export { default as Header } from './Layouts/Header.svelte'
 
-export { default as Projects} from './layouts/Subject/Projects.svelte'
 export { default as LearningJournal} from './layouts/Subject/LearningJournal.svelte'
 export { default as Github_Hyperlinks} from './layouts/Subject/Github_Hyperlinks.svelte'
 export { default as Intro} from './layouts/Subject/Intro.svelte'
 export { default as Keywords} from './layouts/Subject/Keywords.svelte'
+export { default as Details} from './layouts/Subject/Details.svelte'
 
 export { default as Heading } from './components/Heading.svelte'
 export { default as Subjects } from './components/Subjects.svelte'
---
+++ src/layouts/Subject/Details.svelte
@@ -0,0 +1,61 @@
+<script>
+  import {Keywords, Github_Hyperlinks} from '../../index.js'
+  export let data
+  
+  export let themeColor = ''
+
+</script>
+
+<ul>
+  {#each data.Assets as {title, keywords, description, Github}}
+  <li>
+    <details open>
+      <summary>
+        {title}
+      </summary>
+      <div>
+        <Keywords {keywords}/>
+        <h2>About this project</h2>
+        <p>{description}</p>
+
+        <h3>Sources.</h3>
+        <p> here you can find al the sources that are being used in this project</p>
+        {#if Github}
+        <Github_Hyperlinks {Github}/>
+        {:else}
+        <p>No sources available</p>
+        {/if}
+      </div>
+    </details>
+  </li>
+  {/each}
+</ul>
+
+<style>
+
+ul {
+  color: var(--lightmode-color-dark);
+  }
+
+ul li details summary {
+  --font-size:1.2em;
+  font-size: var(--font-size);
+  background-color: var(--lightmode-color-dark);
+  color: var(--lightmode-color-projects);
+  padding: var(--average-padding) 0.5em;
+  transition: 500ms ease-in;
+  font-weight: 300;
+  letter-spacing: .1em;
+}
+
+ul li details summary:hover {
+  font-size: calc(var(--font-size)/100*120);
+  font-weight: 500;
+  letter-spacing: .15em;
+}
+
+ul li details div {
+  padding: var(--average-padding);
+}
+
+</style>
