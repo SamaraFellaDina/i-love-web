d5297e3fa7a2ca3e6ceda98d2efa7f140c72d567
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-01-02 22:42:50 +0100

    maken van een markdown
    ik ben aan het kijken hoe ik een markdown leesbaar maak voor de componenten, ik krijg het tot nu toe nog niet aan de praat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import markdown from '@astrojs/markdown-remark';
 
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), markdown()],
 });
