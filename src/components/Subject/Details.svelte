c9b4a28f289cc25b86a0c5128a4c3a15b22f58c7
Sammy <144009778+SamaraFellaDina@users.noreply.github.com>
2025-01-02 20:46:57 +0100

    dynamisch component
    voor #143 heb ik een component gemaakt die checkt welke titel er is en daarop de pagina opmaakt

---
+++ src/components/Subject/Details.svelte
@@ -0,0 +1,143 @@
+<script>
+  import { Github_Hyperlinks, Keywords } from '../../index.js'
+  export let data
+  let title = data.title
+
+</script>
+
+{#if title === 'Projects'}
+  <ul>
+    {#each data.Assets as { title, description, keywords, Github }}
+      <li>
+        <details open>
+          <summary>
+            {title}
+          </summary>
+          <div>
+            <Keywords {keywords} />
+            <h2>About this project</h2>
+            <p>{description}</p>
+
+            <h3>Sources</h3>
+            <p>Here you can find all the sources that are being used in this project:</p>
+            {#if Github}
+              <Github_Hyperlinks {Github} />
+            {:else}
+              <p>No sources available</p>
+            {/if}
+          </div>
+        </details>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+{#if title === 'Learning Journal'}
+  <ul>
+    {#each data.Assets as { title, description, keywords, Github }}
+      <li>
+        <details open>
+          <summary>
+            {title}
+          </summary>
+          <div>
+            <Keywords {keywords} />
+            <h2>About this project</h2>
+            <p>{description}</p>
+
+            <h3>Sources</h3>
+            <p>Here you can find all the sources that are being used in this project:</p>
+            {#if Github}
+              <Github_Hyperlinks {Github} />
+            {:else}
+              <p>No sources available</p>
+            {/if}
+          </div>
+        </details>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+{#if title === 'We Love Web'}
+  <ul>
+    {#each data.Assets as { title, description, keywords, Github }}
+      <li>
+        <details open>
+          <summary>
+            {title}
+          </summary>
+          <div>
+            <Keywords {keywords} />
+            <h2>About this project</h2>
+            <p>{description}</p>
+
+            <h3>Sources</h3>
+            <p>Here you can find all the sources that are being used in this project:</p>
+            {#if Github}
+              <Github_Hyperlinks {Github} />
+            {:else}
+              <p>No sources available</p>
+            {/if}
+          </div>
+        </details>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+{#if title === 'About me'}
+  <ul>
+    {#each data.Assets as { title, description, keywords, Github }}
+      <li>
+        <details open>
+          <summary>
+            {title}
+          </summary>
+          <div>
+            <Keywords {keywords} />
+            <h2>About this project</h2>
+            <p>{description}</p>
+
+            <h3>Sources</h3>
+            <p>Here you can find all the sources that are being used in this project:</p>
+            {#if Github}
+              <Github_Hyperlinks {Github} />
+            {:else}
+              <p>No sources available</p>
+            {/if}
+          </div>
+        </details>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+<style>
+
+ ul {
+    background: var(--lightmode-color-projects);
+    color: var(--lightmode-color-dark);
+  }
+  ul li details summary {
+    --font-size:1.2em;
+    font-size: var(--font-size);
+    background-color: var(--lightmode-color-dark);
+    color: var(--lightmode-color-projects);
+    padding: var(--average-padding) 0.5em;
+    transition: 500ms ease-in;
+    font-weight: 300;
+    letter-spacing: .1em;
+  }
+
+ ul li details summary:hover {
+  font-size: calc(var(--font-size)/100*120);
+  font-weight: 500;
+  letter-spacing: .15em;
+
+}
+
+ ul li details div {
+  padding: var(--average-padding);
+}
+</style>
--- src/components/Subject/Projects.svelte
+++ src/components/Subject/Projects.svelte
@@ -5,31 +5,6 @@ const data = Archive.archive[0]
 
 <section>
   <Intro {data} />
-  <ul>
-    {#each data.Assets as {title, description, keywords, Github}}
-    <li>
-
-      <details open>
-        <summary>
-          {title}
-        </summary>
-        <div>
-          <Keywords {keywords}/>
-          <h2>About this project</h2>
-          <p>{description}</p>
-
-          <h3>Sources.</h3>
-          <p> here you can find al the sources that are being used in this project</p>
-          {#if Github}
-          <Github_Hyperlinks {Github}/>
-          {:else}
-          <p>No sources available</p>
-          {/if}
-        </div>
-      </details>
-    </li>
-    {/each}
-  </ul>
 </section>
 
 <style>
@@ -40,31 +15,6 @@ const data = Archive.archive[0]
     position: relative;
   }
 
-  section ul {
-    background: var(--lightmode-color-projects);
-    color: var(--lightmode-color-dark);
-  }
-  section ul li details summary {
-    --font-size:1.2em;
-    font-size: var(--font-size);
-    background-color: var(--lightmode-color-dark);
-    color: var(--lightmode-color-projects);
-    padding: var(--average-padding) 0.5em;
-    transition: 500ms ease-in;
-    font-weight: 300;
-    letter-spacing: .1em;
-}
-
-section ul li details summary:hover {
-  font-size: calc(var(--font-size)/100*120);
-  font-weight: 500;
-  letter-spacing: .15em;
-
-}
-
-section ul li details div {
-  padding: var(--average-padding);
-}
 
 
 </style>
